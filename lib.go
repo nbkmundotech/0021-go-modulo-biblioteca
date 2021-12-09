8e2a423abaa96aa467546fb7c057b256864a7385
nbkhope <ryumemaru@yahoo.com>
2021-12-08 23:54:39 -0500

    Adicionar comentarios

--- lib.go
+++ lib.go
@@ -1,10 +1,12 @@
 package biblioteca
 
+// Livro representa um livro
 type Livro struct {
 	publicadoEm int
 	titulo string
 }
 
+// NovoLivro constroi um livro
 func NovoLivro(publicadoEm int, titulo string) *Livro {
 	return &Livro{
 		publicadoEm: publicadoEm,
